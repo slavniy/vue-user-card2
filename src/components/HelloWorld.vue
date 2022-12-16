c580b214d7101e4177a8cee4ab54266370067415
Viktor92 <larionov92spb@gmail.com>
2022-12-17 00:24:34 +0300

    create user card component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,25 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="user-card">
+        <img src="img/70.jpg" class="user-photo">
+        <p class="user-nickname">romashka</p>
+        <p class="user-name">
+            Иванов Иван<br>
+            Иванович
+        </p>
+        <hr>
+        <p class="user-info-item">
+            <img src="img/location.png">
+            Москва, Юбилейная 50
+        </p>
+        <p class="user-info-item">
+            <img src="img/mail.png">
+            coldrabbit48@example.com
+        </p>
+        <p class="user-info-item">
+            <img src="img/phone.png">
+            +7-495-266-57-34
+        </p>
+    </div>
 </template>
 
 <script>
@@ -41,18 +33,48 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.user-card {
+    width: 300px;
+    padding: 20px;
+    border-radius: 10px;
+    margin-top: 100px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    box-shadow: 0 0 15px 0 rgba(50, 50, 50, 0.2);
+    text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.user-card .user-photo {
+    box-sizing: border-box;
+    height: 150px;
+    margin-top: -100px;
+    border-radius: 50%;
+    border: 3px solid #ccc;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.user-card .user-name {
+    font-size: 30px;
+    font-weight: 900;
+    line-height: 1.4;
+    margin: 5px 0;
 }
-a {
-  color: #42b983;
+
+.user-card .user-nickname {
+    color: #a0a9b3;
+    font-size: 18px;
+    margin: 5px 0;
+    font-family: monospace;
+}
+
+.user-card hr {
+    height: 4px;
+    width: 50px;
+    margin: 10px auto;
+    background-color: #2c3e50;
+}
+
+.user-card .user-info-item img {
+    vertical-align: middle;
+    margin-right: 5px;
 }
 </style>
