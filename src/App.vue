2fb7870a48bc79f82453fa9d0231d15b9c2f639d
Viktor92 <larionov92spb@gmail.com>
2022-12-17 00:25:20 +0300

    create user card component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserCardComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserCardComponent from './components/UserCardComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserCardComponent
   }
 }
 </script>
